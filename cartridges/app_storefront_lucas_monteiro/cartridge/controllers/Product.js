52b6d40201595748d8cc9fdaae46cb3770e165a8
Lucas Monteiro <lucas.monteiro@alphasquad.cx>
2022-08-25 14:54:07 -0300

    ultimo commit

--- cartridges/app_storefront_lucas_monteiro/cartridge/controllers/Product.js
+++ cartridges/app_storefront_lucas_monteiro/cartridge/controllers/Product.js
@@ -5,20 +5,26 @@ server.extend(module.superModule)
 
 server.append('Show', function (req, res, next){
     var viewData = res.getViewData();
-    var ProductMgr = require('dw/catalog/ProductMgr');
-    var product = ProductMgr.getProduct(req.querystring.pid)
-    var listPrice = viewData.product.price.list.value;
-    var salesPrice = viewData.product.price.sales.value;
-    var percentageSales =  Math.round((salesPrice * 100)/listPrice);
-    var percentageDiscount = 100 - percentageSales;
-    var productID = viewData.product.id;
-    var ProductSearchModel = require('dw/catalog/ProductSearchModel');
     var CatalogMgr = require('dw/catalog/CatalogMgr');
-    var productCategoryID = product.allCategories[0].ID;
-    var getCategory = CatalogMgr.getCategory(productCategoryID)
-    var sortingRule = CatalogMgr.getSortingRule('price-low-to-high')
-    viewData.percentageDiscount = percentageDiscount;
+    var ProductMgr = require('dw/catalog/ProductMgr');
+    var ProductSearchModel = require('dw/catalog/ProductSearchModel');
+    var PagingModel = require('dw/web/PagingModel');
 
+    var product = ProductMgr.getProduct(req.querystring.pid)
+    var category = product.getPrimaryCategory();
+    var sortingRule = CatalogMgr.getSortingRule('price-low-to-high')
+
+    var apiProductSearch = new ProductSearchModel()
+    apiProductSearch.setSortingRule(sortingRule)
+    apiProductSearch.setCategoryID(category.ID)
+    apiProductSearch.search()
+    
+    var pagingModel = new PagingModel(apiProductSearch.getProductSearchHits(), apiProductSearch.count)
+    pagingModel.setStart(0)
+    pagingModel.setPageSize(4)
+    
+    var products = pagingModel.pageElements.asList()
+    viewData.products = products
     res.setViewData(viewData)
 
     next()
