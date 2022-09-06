523f59ca381623169391b3086a01ba4f3dd34c09
Lucas-Monteiro07 <lucasmontesilva@gmail.com>
2022-09-06 11:08:04 -0300

    Email teste 2

--- cartridges/app_storefront_lucas_monteiro/cartridge/controllers/Newsletter.js
+++ cartridges/app_storefront_lucas_monteiro/cartridge/controllers/Newsletter.js
@@ -3,7 +3,7 @@
 var server = require('server');
 
 
-server.get('Show', csrfProtection.generateToken, function (req, res, next){
+server.get('Show', function (req, res, next){
     var URLUtils = require('dw/web/URLUtils');
     var form = server.forms.getForm('newsletter');
     res.render('/newsletter/newsletter', {
@@ -14,26 +14,29 @@ server.get('Show', csrfProtection.generateToken, function (req, res, next){
  });
 
 server.post('Submit', function (req, res, next){
+    var viewData = res.getViewData();
     var fields = req.form;
     var name = fields.firstName;
     var lastname = fields.lastName;
     var email = fields.email;
-    var teste = 'checkout/confirmation/newsletter-email.isml'
     var emailObj = {
         to: email,
         subject: "Confirmação de Newsletter",
         from: "noreply@salesforce.com"
     }
     var context = {
-        firstName: firstName,
-        lastName: lastName
+        firstName: name,
+        lastName: lastname
     }
-
+    
+    var template = 'checkout/confirmation/newsletter-email'
     var emailHelper = require('*/cartridge/scripts/helpers/emailHelpers')
     if (emailHelper.validateEmail(email)){
-        emailHelper.send(emailObj, teste, context)
+        emailHelper.send(emailObj, template, context)
     }
 
+    viewData.template = template;
+    res.setViewData(viewData);
     next()
 })
 
